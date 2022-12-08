54ac281a3eb6f88510a1c7d36ebdd8b16a53e188
Alex Williams <awilliams@alexwilliams.ca>
2022-12-09 07:15:34 +0900

    Add devlog 17. Fixing UART

--- fiveforths.s
+++ fiveforths.s
@@ -183,7 +183,7 @@ uart_init:
     # enable the RCU clocks
     li t0, 0x40021000                       # load base address of the RCU
     lw t1, 0x18(t0)                         # load value from the APB2 enable register (RCU_APB2EN)
-    li t2, (1 << 14) | (1 << 2)             # set USART0EN (bit 14), PAEN (bit 2)
+    li t2, (1 << 14) | (1 << 2) | (1 << 0)  # set USART0EN (bit 14), PAEN (bit 2), AFEN (bit 0)
     or t1, t1, t2                           # add the enabled bits to the existing RCU_APB2EN value
     sw t1, 0x18(t0)                         # store value in RCU_APB2EN register at offset 0x18
 
@@ -199,8 +199,8 @@ uart_init:
     # disable hardware flow control, half-duplex, etc
     sw zero, 0x14(t0)                       # store the value in the USART Control register 2 (USART_CTL2)
 
-    # enable receiver, transmitter, uart
-    li t1, (1 << 2) | (1 << 3) | (1 << 13)  # set REN (bit 2), TEN (bit 3), UEN (bit 13)
+    # enable receiver, transmitter, uart, interrupts
+    li t1, (1 << 2) | (1 << 3) | (1 << 5) | (1 << 6) | (1 << 7) | (1 << 13) # set REN, TEN, RBNEIE, TCIE, TBEIE, UEN bits to 1
     sw t1, 0x0C(t0)                         # store the value in the USART Control register 0 (USART_CTL0)
 
 uart_done:
@@ -208,12 +208,12 @@ uart_done:
 
 # Initialize the GPIO
 gpio_init:
-    # configure TX on pin 9 of port A (0b0011)
+    # configure TX on pin 9 of port A (0b1011)
     li t0, 0x40010800                       # load base address of GPIOA
     lw t1, 0x04(t0)                         # load value from the Port control register 1 (GPIOA_CTL1)
     li t2, 0xfffff00f                       # load bitmask to clear 8 bits (MD9[1:0],CTL9[1:0],MD10[1:0],CTL10[1:0])
     and t1, t1, t2                          # clear the bits (TX 4,5,6,7) and (RX 8,9,10,11)
-    or t1, t1, (1 << 4) | (1 << 5) | (0 << 6) | (0 << 7) # set the bits 4,5,6,7 (output push-pull, max speed 50MHz)
+    or t1, t1, (1 << 4) | (1 << 5) | (0 << 6) | (1 << 7) # set the bits 4,5,6,7 (afio push-pull, max speed 50MHz)
 
     # configure RX on pin 10 of port A (0b0100)
     li t2, (0 << 8) | (0 << 9) | (1 << 10) | (0 << 11)  # load the bits 8,9,10,11
